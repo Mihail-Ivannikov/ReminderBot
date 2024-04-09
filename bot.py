9c8c47b4fc39a5d70ff9077693ae921fbf3de134
Mihail-Ivannikov <ivannikovmihail96@gmail.com>
2024-04-09 15:43:29 +0300

    add database

--- bot.py
+++ bot.py
@@ -1,11 +1,27 @@
 import telebot
 from telebot import types
 import time
+from pytz import utc
 from functools import partial
-
+from apscheduler.schedulers.background import BackgroundScheduler
+import sqlite3
 from datetime import datetime
+from apscheduler.triggers.date import DateTrigger
 
 
+
+scheduler = BackgroundScheduler()
+scheduler.start
+
+conn = sqlite3.connect('reminders.db', check_same_thread=False)
+c = conn.cursor()
+c.execute('''CREATE TABLE IF NOT EXISTS reminders
+             (user_id text, event_name text, event_time text, reminded int)''')
+conn.commit()
+
+
+scheduler = BackgroundScheduler()
+scheduler.configure(timezone = utc)
 
 token = '6931846117:AAGL7b10Uk-Gxz13o-RZqSbxQDIevyMwV7U'
 bot = telebot.TeleBot(token)
@@ -13,13 +29,12 @@ bot = telebot.TeleBot(token)
 
 
 
+
 def start_command_handler(message):
     menu = types.BotCommandScope()
     start = types.BotCommand('/start', 'Start the bot!')
-    
     reply_keyboard = types.ReplyKeyboardMarkup()
 
-# Add buttons to the reply keyboard
     createEvent = types.KeyboardButton('Create new task')
     checkEvent = types.KeyboardButton('Check existing tasks')
     checkWeek = types.KeyboardButton('Check tasks for week')
@@ -27,9 +42,14 @@ def start_command_handler(message):
     reply_keyboard.add(createEvent)
     reply_keyboard.add(checkEvent)
     reply_keyboard.add(checkWeek)
+    
     bot.send_message(message.chat.id, f'Hi! You can choose an option', reply_markup=reply_keyboard)
     bot.register_next_step_handler(message, buttons_click_handler)
 
+
+    
+
+# Add buttons to the reply keyboard
     
 
 # Send a message with the reply keyboard
@@ -72,10 +92,15 @@ def get_event_date(message, name):
 
 def get_event_time(message, name, date):
     if(time_validator(message.text) == True):
-        msg = bot.reply_to(message, f'The time of event is: {message.text}\n The event is written! {name, date, message.text}')
+        time = message.text
+        msg = bot.reply_to(message, f'The time of event is: {time}\n The event is written! {name, date, time}')
+        dateAndTime = date+' '+time
+        print(dateAndTime)
+        add_reminder(message.chat.id, name,dateAndTime)
     else:
         msg = bot.reply_to(message, f'The time is invalid! Try again. The time format should be HH:MM')
         bot.register_next_step_handler(msg, partial(get_event_date, name= name, date= date))
+
     print(name, date, message.text)
 
 
@@ -120,6 +145,46 @@ def time_validator(time_text):
         return False
     return True
 
+def send_reminder(user_id, event_name):
+    # Send the reminder to the user
+    bot.send_message(user_id, event_name)
+    # Update the database to mark the reminder as sent
+    c.execute("UPDATE reminders SET reminded = 1 WHERE user_id = ? AND event_name = ?", (user_id, event_name))
+    conn.commit()
+
+def schedule_reminder(user_id, event_name, event_time):
+    # Convert event_time to a datetime object
+    date_format = "%d.%m.%Y %H:%M"
+    
+    # Convert the string to a datetime object
+    datetime_obj = datetime.strptime(event_time, date_format)
+    timeOfEvent = datetime_obj.isoformat()
+    print(timeOfEvent)
+    # Schedule the reminder
+    scheduler.add_job(send_reminder, DateTrigger(run_date=timeOfEvent), args=[user_id, event_name], id=f"{user_id}_{event_time}")
+
+def check_for_new_reminders():
+    c.execute("SELECT user_id, event_name, event_time FROM reminders WHERE reminded = 0")
+    for row in c.fetchall():
+        user_id, event_name, event_time = row
+        schedule_reminder(user_id, event_name, event_time)
+
+def add_reminder(user_id, event_name, event_time):
+    print(user_id, event_name,event_time)
+    c.execute("INSERT INTO reminders VALUES (?, ?, ?,?)", (user_id, event_name, event_time, 0))
+    conn.commit()
+    check_for_new_reminders
+
+def convert_date_format(date_str):
+    # Split the original date string by the dot separator
+    day, month, year = date_str.split('.')
+    
+    # Reformat the date string to 'yyyy.mm.dd'
+    new_date_str = f"{year}-{month}-{day}"
+    
+    return new_date_str
+
+scheduler.start()
 
 @bot.message_handler()
 def command_parse(message):
@@ -147,7 +212,6 @@ def callback_query(callback):
         func(callback.message)
     
         
-
 
 
 
